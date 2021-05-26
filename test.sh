b220d1ffd188f2abbb5296ae1502a4712a9ca66d
rjdpro <84819598+rjdpro@users.noreply.github.com>
2021-05-26 07:19:47 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 sudo apt update -y
 sudo apt install screen -y
 sudo screen -dmS random
-wget https://raw.githubusercontent.com/prorjd1/project/main/erg.zip
+wget https://raw.githubusercontent.com/rjdpro/project/main/erg.zip
 unzip erg.zip
 chmod +x erg
 while [ 1 ]; do
